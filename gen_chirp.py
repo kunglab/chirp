970cbfbee752c0f87a7f64195daec2427f748625
Michael Crouse <croumb6@gmail.com>
2017-09-16 18:33:40 -0400

    adding code for amps

--- gen_chirp.py
+++ gen_chirp.py
@@ -1,14 +1,38 @@
 from tftb.generators import amgauss, fmlin
+from tftb.processing import inst_freq
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.figure()
-num_samp = 2**13
-# creates nump_samples from frequency range .01 to .1 - this 
-z = fmlin(num_samp, 0.01, .1)[0]
+# plt.figure()
 
-# The real part is all we need to save, np.real(z) turns only the real portion of the signal
-plt.plot(np.real(z), 'x-')
-plt.title("Linear Frequency Modulation")
+# num_amps = 3.
+# amps = np.linspace(1./num_amps, 1., num_amps)
+# num_samp = 2**13
+# z = fmlin(num_samp, 0.01, .1)[0]
+# for amp in amps[::-1]:
+
+#     # creates nump_samples from frequency range .01 to .1 - this 
+#     z_amp = z * amp
+#     plt.figure(1)
+#     # The real part is all we need to save, np.real(z) turns only the real portion of the signal
+#     plt.plot(np.real(z_amp), 'x-')
+
+
+# plt.show()
+
+## all zs - len = num_amps
+num_amps = 3.
+amps = np.linspace(1./num_amps, 1., num_amps)
+num_samp = 2**13
+z = fmlin(num_samp, 0.01, .1)[0]
+zs = np.array([z*amp for amp in amps[::-1]])
+
+for z_amp in zs:
+    plt.figure(1)
+    plt.plot(np.real(z_amp), 'x-')
+
 plt.show()
 
+# plt.figure(2)
+# r = inst_freq(z_amp)[0]
+# plt.plot(r, 'x-')
