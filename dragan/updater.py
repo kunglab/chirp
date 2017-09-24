cf79fd5407e650ab95fc05942bb19411bd43fa72
Brad McDanel <mcdabw6@gmail.com>
2017-09-23 23:25:40 -0400

    added discriminator/encoder joint model

--- dragan/updater.py
+++ dragan/updater.py
@@ -96,8 +96,6 @@ class LabeledUpdater(chainer.training.StandardUpdater):
 
         y_fake_class = F.reshape(z[:, -self.n_labels:, 0, 0], (-1, self.n_labels))
         y_real_class = xp.asarray(y).astype('f').reshape(-1, self.n_labels)
-        # y_fake_class.data = y_fake_class.data.astype(xp.int32)
-        # y_real_class = y_real_class.astype(xp.int32)
 
         loss_g_class = self.class_error_f(yh_fake_class, y_fake_class)
         loss_gen = self.adv_lam*(F.sum(F.softplus(-yh_fake)) / batchsize) + loss_g_class
@@ -112,12 +110,7 @@ class LabeledUpdater(chainer.training.StandardUpdater):
 
         yh_perturb, yh_perturb_class = self.dis(x_perturb)
         dydx = self.dis.differentiable_backward(xp.ones_like(yh_perturb), xp.ones_like(yh_perturb_class))
-        #dydx = self.dis.differentiable_backward(yh_perturb, yh_perturb_class)
-        #dydx = F.sqrt(F.sum(dydx ** 2))
         loss_gp = self.gp_lam * F.mean_squared_error(dydx, xp.ones_like(dydx.data))
-        #loss_gp = self.gp_lam * ((dydx - 1) ** 2)
-        #print (dydx - 1) ** 2
-
 
         loss_dis  = self.adv_lam*(F.sum(F.softplus(-yh_real)) / batchsize)
         loss_dis += self.adv_lam*(F.sum(F.softplus(yh_fake))  / batchsize)
@@ -133,5 +126,86 @@ class LabeledUpdater(chainer.training.StandardUpdater):
             'loss_gen_c': loss_g_class,
             'loss_dis': loss_dis,
             'loss_dis_c': loss_d_class,
+            'loss_gp': loss_gp
+        })
+
+
+class AlphaUpdater(chainer.training.StandardUpdater):
+    def __init__(self, *args, **kwargs):
+        self.gen, self.dis = kwargs.pop('models')
+        self.gp_lam = kwargs.pop('gp_lam')
+        self.adv_lam = kwargs.pop('adv_lam')
+        self.class_error_f = kwargs.pop('class_error_f')
+        self.n_labels = kwargs.pop('n_labels')
+        self.n_noise = kwargs.pop('n_noise')
+        super(AlphaUpdater, self).__init__(*args, **kwargs)
+    
+    def update_core(self):
+        gen_optimizer = self.get_optimizer('opt_gen')
+        dis_optimizer = self.get_optimizer('opt_dis')
+        xp = self.gen.xp
+
+        batch = self.get_iterator('main').next()
+        batchsize = len(batch)
+        x, y = [], []
+        for i in range(batchsize):
+            xi, yi = batch[i]
+            x.append(np.asarray(xi).astype("f"))
+            y.append(yi)
+        z = Variable(xp.asarray(self.gen.make_hidden(batchsize)))
+        x_fake = self.gen(z)
+        x_real = (xp.asarray(x))
+        std_x_real = xp.std(x_real, axis=0, keepdims=True)
+        rnd_x = xp.random.uniform(0, 1, x_real.shape).astype('f')
+        x_perturb = Variable(x_real + 0.5 * rnd_x * std_x_real)
+
+        yh_fake, yh_fake_z = self.dis(x_fake)
+        yh_real, yh_real_z = self.dis(x_real)
+        yh_fake_class = yh_fake_z[:, self.n_noise:]
+        yh_fake_class.data = xp.ascontiguousarray(yh_fake_class.data)
+        yh_fake_noise = yh_fake_z[:, :self.n_noise]
+        yh_fake_noise.data = xp.ascontiguousarray(yh_fake_noise.data)
+        yh_real_class = yh_real_z[:, self.n_noise:]
+        yh_real_class.data = xp.ascontiguousarray(yh_real_class.data)
+        yh_real_noise = yh_real_z[:, :self.n_noise]
+        yh_real_noise.data = xp.ascontiguousarray(yh_real_noise.data)
+
+        y_fake_noise = F.reshape(z[:, :self.n_noise, 0, 0], (-1, self.n_noise))
+        y_fake_class = F.reshape(z[:, self.n_noise:, 0, 0], (-1, self.n_labels))
+        y_real_class = xp.asarray(y).astype('f').reshape(-1, self.n_labels)
+
+        loss_g_class = self.class_error_f(yh_fake_class, y_fake_class)
+        loss_gen = self.adv_lam*(F.sum(F.softplus(-yh_fake)) / batchsize)
+        #loss_gen = self.adv_lam*(F.sum(F.softplus(-yh_fake)) / batchsize) + loss_g_class
+
+        self.gen.cleargrads()
+        loss_gen.backward()
+        gen_optimizer.update()
+
+        x_fake.unchain_backward()
+
+        loss_d_class = self.class_error_f(yh_real_class, y_real_class)
+        loss_noise = F.mean_absolute_error(yh_fake_noise, y_fake_noise)
+
+        yh_perturb, yh_perturb_class = self.dis(x_perturb)
+        dydx = self.dis.differentiable_backward(xp.ones_like(yh_perturb), xp.ones_like(yh_perturb_class))
+        loss_gp = self.gp_lam * F.mean_squared_error(dydx, xp.ones_like(dydx.data))
+
+        loss_dis  = self.adv_lam*(F.sum(F.softplus(-yh_real)) / batchsize)
+        loss_dis += self.adv_lam*(F.sum(F.softplus(yh_fake))  / batchsize)
+        #loss_dis += loss_d_class
+        loss_dis += loss_noise
+        loss_dis += loss_gp
+ 
+        self.dis.cleargrads()
+        loss_dis.backward()
+        dis_optimizer.update()
+
+        chainer.reporter.report({
+            'loss_gen': loss_gen,
+            'loss_gen_c': loss_g_class,
+            'loss_dis': loss_dis,
+            'loss_dis_c': loss_d_class,
+            'loss_noise': loss_noise,
             'loss_gp': loss_gp
         })
