7166f0d5748850bfdd3d74333e1d5160a028831e
Marcus Comiter <marcuscomiter@gmail.com>
2017-09-25 13:18:54 -0400

    Cleaned version of generate_alpha_ac_reg_style.py

--- generate_alpha_ac_reg_style.py
+++ generate_alpha_ac_reg_style.py
@@ -25,12 +25,6 @@ import utilities as util
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-
-## Alex net ##
-
 
 def slerp(p0, p1, t):
     omega = np.arccos(np.dot(p0/np.linalg.norm(p0), p1/np.linalg.norm(p1)))
@@ -42,36 +36,28 @@ def slerpn(p0, p1, pts=10, min_t=0.0, max_t=1.0):
                      for i in np.linspace(min_t, max_t, pts)])
 
 
-
-
-model_map = {"AlexStock": Alex, "VTCNN2" : VTCNN2}
-
 parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-parser.add_argument('--batchsize', '-b', type=int, default=100,
-                    help='Number of images in each mini-batch')
-parser.add_argument('--epoch', '-e', type=int, default=20,
-                    help='Number of sweeps over the dataset to train')
 parser.add_argument('--gpu', '-g', type=int, default=-1,
                     help='GPU ID (negative value indicates CPU)')
-parser.add_argument('--model', '-m', default='result_classifier',
-                    help='Path to saved model')
+# parser.add_argument('--model', '-m', default='result_classifier',
+#                     help='Path to saved model')
 parser.add_argument('--out', '-o', default='result_classifier',
                     help='Directory to output the result')
-parser.add_argument('--unit', '-u', type=int, default=1000,
-                    help='Number of units')
-parser.add_argument('--model_type', '-t', type=str, default="AlexStock",
-                    help='Which Model to run (AlexStock, VTCNN2)')
 args = parser.parse_args()
 
 
-save_dir = 'ac_reg_images_6/'
+if not os.path.exists(args.out):
+    os.makedirs(args.out)
+
+
 save_npzs = True
 
-###### SETUP DATASET #####
+#######################################
+###### SETUP DATASET AND MODEL ########
+#######################################
 noise_levels = [6,8,16,18]
 style_dataset = dataset.RFModLabeled(class_set=['8PSK'], noise_levels=noise_levels, test=False)
 train_max = np.max(np.abs(style_dataset.xs))
-
 
 def make_hidden(n_hidden, batchsize):
     zs = np.random.randn(batchsize, n_hidden, 1, 1).astype(np.float32)
@@ -95,7 +81,12 @@ if args.gpu >= 0:
 
 xp = generator.xp
 
-evaluation_noise_levels = [6,8,10,12,14,16,18]#range(6, 20, 2)
+
+
+#####################################################################
+####### WRITE OUT PREDICTIONS FOR CLASSIFIER ACCURACY CHECK #########
+######################################################################
+evaluation_noise_levels = [6,8,10,12,14,16,18]
 for noise_level in evaluation_noise_levels:
 
     style_dataset = dataset.RFModLabeled(class_set=['8PSK'], noise_levels=[noise_level], test=False)
@@ -103,19 +94,19 @@ for noise_level in evaluation_noise_levels:
     style_dataset.xs /= train_max
 
     query_image = style_dataset.xs[20:21]
-    # print query_image.shape
-    # assert False
-
-
 
     query_image = xp.array(query_image)
     _, s_vector = discriminator(query_image)
 
     print "True: %f, Predicted: %f" % (noise_level, s_vector.data[0][-1])
-    print s_vector.data[0]
-    continue
+    # print s_vector.data[0]
 
-evaluation_noise_levels = [6,12,18]#range(6, 20, 2)
+
+
+########################################################################
+####### GENERATE ORIGINAL AND ENCODED-RECONSTRUCTED IMAGES #############
+########################################################################
+evaluation_noise_levels = [6,12,18]
 for noise_level in evaluation_noise_levels:
     style_dataset = dataset.RFModLabeled(class_set=['8PSK'], noise_levels=[noise_level], test=False)
 
@@ -130,32 +121,30 @@ for noise_level in evaluation_noise_levels:
     x = query_image
     xh = query_image_h
 
-
-    # query_image = chainer.cuda.to_cpu(query_image.data)
-    # query_image_h = chainer.cuda.to_cpu(query_image_h.data)
-
     x = chainer.cuda.to_cpu(x)
     x = x*train_max
 
     xh = chainer.cuda.to_cpu(xh.data)
-    # xh = xh.reshape(xh.shape[0], xh.shape[2], xh.shape[3])
     xh = xh*train_max
 
     plt.figure()
     plt.plot(x.reshape(x.shape[2],x.shape[3]).T)
     plt.ylim([-3,3])
-    plt.savefig(save_dir + 'SNR%d_original.png' % (noise_level))
+    plt.savefig(os.path.join(args.out, 'SNR%d_original.png' % (noise_level)))
     plt.figure()
-    plt.plot(xh.reshape(x.shape[2],x.shape[3]).T)
+    plt.plot(xh.reshape(xh.shape[2],xh.shape[3]).T)
     plt.ylim([-3,3])
-    plt.savefig(save_dir + 'SNR%d_recon.png' % (noise_level))
+    plt.savefig(os.path.join(args.out, 'SNR%d_recon.png' % (noise_level)))
 
     if save_npzs:
-        np.savez(save_dir + 'SNR%d_original.npz' % (noise_level), x.reshape(x.shape[2],x.shape[3]).T)
-        np.savez(save_dir + 'SNR%d_recon.npz' % (noise_level), xh.reshape(xh.shape[2],xh.shape[3]).T)
+        np.savez(os.path.join(args.out, 'SNR%d_original.npz' % (noise_level)), x.reshape(x.shape[2],x.shape[3]).T)
+        np.savez(os.path.join(args.out, 'SNR%d_recon.npz' % (noise_level)), xh.reshape(xh.shape[2],xh.shape[3]).T)
 
 
-###### ANIMATION
+
+############################################################################################
+####### GENERATE ANIMATION FIXED SNR INTERPOLATE IN Z SPACE BETWEEN TWO QUERY POINTS ####### 
+############################################################################################
 evaluation_noise_levels = [6,12,18]#range(6, 20, 2)
 for noise_level in evaluation_noise_levels:
     style_dataset = dataset.RFModLabeled(class_set=['8PSK'], noise_levels=[noise_level], test=False)
@@ -197,20 +186,19 @@ for noise_level in evaluation_noise_levels:
         return line1, line2
 
 
-    if not os.path.exists('alpha_animations'):
-        os.makedirs('alpha_animations')
-
-    #plt.axis('off')
     plt.tight_layout()
     ani = animation.FuncAnimation(fig, func, init_func=init, frames=np.arange(len(z))+10, interval=100, blit=True)
-    ani.save(save_dir + 'SNR%d_animation.gif' % (noise_level), dpi=80, writer='imagemagick')
+    ani.save(os.path.join(args.out, 'SNR%d_animation.gif' % (noise_level)), dpi=80, writer='imagemagick')
     plt.close(fig)
 
     if save_npzs:
-        np.savez(save_dir + 'SNR%d_animation.npz' % (noise_level), x)
+        np.savez(os.path.join(args.out, 'SNR%d_animation.npz' % (noise_level)), x)
 
 
-###### interpolate over SNR
+
+##########################################################################
+###### GENERATE ANIMATION FIXED QUERY POINT INTERPOLATE OVER SNR #########
+##########################################################################
 style_dataset = dataset.RFModLabeled(class_set=['8PSK'], noise_levels=[18], test=False)
 
 style_dataset.xs /= train_max
@@ -261,11 +249,11 @@ def func(i):
 #plt.axis('off')
 plt.tight_layout()
 ani = animation.FuncAnimation(fig, func, init_func=init, frames=np.arange(len(z))+10, interval=100, blit=True)
-ani.save(save_dir + 'SNR_interpolation.gif', dpi=80, writer='imagemagick')
+ani.save(os.path.join(args.out, 'SNR_interpolation.gif'), dpi=80, writer='imagemagick')
 plt.close(fig)
 
 if save_npzs:
-        np.savez(save_dir + 'SNR_interpolation.npz', x)
+        np.savez(os.path.join(args.out, 'SNR_interpolation.npz'), x)
     
 
 assert False
