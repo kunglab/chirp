9c6fd98706be72cbb2bfea52d03f592c380b92b8
Michael Crouse <croumb6@gmail.com>
2017-09-27 11:32:48 -0400

    re parameterizing alex net - brad...

--- common/net.py
+++ common/net.py
@@ -290,18 +290,29 @@ class LabeledDiscriminatorJoined(chainer.Chain):
 
 
 class Alex(chainer.Chain):
-    def __init__(self, output_dim):
+    def __init__(self, output_dim, init_weights=False, filter_height=1):
         super(Alex, self).__init__()
         self.output_dim = output_dim
+        self.filter_height = filter_height
         with self.init_scope():
-            self.conv1 = L.Convolution2D(None,  96, (1, 3), 1, (0, 1))
-            self.conv2 = L.Convolution2D(None, 256, (1, 3), 1, (0, 1))
-            self.conv3 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1))
-            self.conv4 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1))
-            self.conv5 = L.Convolution2D(None, 256, (1, 3), 1, (0, 1))
-            self.fc6 = L.Linear(None, 4096)
-            self.fc7 = L.Linear(None, 4096)
-            self.fc8 = L.Linear(None, output_dim)
+            if init_weights:
+                self.conv1 = L.Convolution2D(None,  96, (1, 3), 1, (0, 1), initialW=GlorotNormal())
+                self.conv2 = L.Convolution2D(None, 256, (filter_height, 3), 1, (0, 1), initialW=GlorotNormal())
+                self.conv3 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1), initialW=GlorotNormal())
+                self.conv4 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1), initialW=GlorotNormal())
+                self.conv5 = L.Convolution2D(None, 256, (1, 3), 1, (0, 1), initialW=GlorotNormal())
+                self.fc6 = L.Linear(None, 4096, initialW=HeNormal())
+                self.fc7 = L.Linear(None, 4096, initialW=HeNormal())
+                self.fc8 = L.Linear(None, output_dim, initialW=HeNormal())
+            else:
+                self.conv1 = L.Convolution2D(None,  96, (1, 3), 1, (0, 1))
+                self.conv2 = L.Convolution2D(None, 256, (filter_height, 3), 1, (0, 1))
+                self.conv3 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1))
+                self.conv4 = L.Convolution2D(None, 384, (1, 3), 1, (0, 1))
+                self.conv5 = L.Convolution2D(None, 256, (1, 3), 1, (0, 1))
+                self.fc6 = L.Linear(None, 4096)
+                self.fc7 = L.Linear(None, 4096)
+                self.fc8 = L.Linear(None, output_dim)
 
     def __call__(self, x):
         h = F.max_pooling_2d(F.local_response_normalization(
@@ -310,7 +321,7 @@ class Alex(chainer.Chain):
             F.relu(self.conv2(h))), (1,3), stride=1)
         h = F.relu(self.conv3(h))
         h = F.relu(self.conv4(h))
-        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), (1,3), stride=2)
         h = F.dropout(F.relu(self.fc6(h)))
         h = F.dropout(F.relu(self.fc7(h)))
         h = self.fc8(h)
